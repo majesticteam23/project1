f304cd50cfe16e7c1baf861444555d4225ea526c
majesticteam23 <m9113826@gmail.com>
2023-12-23 07:13:20 +0530

    first commit

--- my-appone/src/App.js
+++ my-appone/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Team majeasstic 2023
         </p>
         <a
           className="App-link"
@@ -18,7 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
-      
+
     </div>
   );
 }
